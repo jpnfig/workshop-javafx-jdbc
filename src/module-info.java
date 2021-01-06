7ad8a13473351141ff4d9674afe161693746318a
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2021-01-06 13:30:22 -0300

    Main view

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,11 @@
 module workshop_javafx_jdbc {
+	
+	requires javafx.fxml;
+	requires javafx.graphics;
 	requires javafx.controls;
 	
-	opens application to javafx.graphics, javafx.fxml;
+//	exports gui;    
+
+    opens gui to javafx.fxml;
+	opens application to javafx.graphics, javafx.controls, javafx.fxml;
 }
