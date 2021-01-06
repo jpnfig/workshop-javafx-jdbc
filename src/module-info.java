53089e6b0a1b34259c15b4e57a52013091d85ba7
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2021-01-06 16:05:45 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -4,8 +4,8 @@ module workshop_javafx_jdbc {
 	requires javafx.graphics;
 	requires javafx.controls;
 	
-//	exports gui;    
+	exports gui;    
 
-//  opens gui to javafx.fxml;
+    opens gui to javafx.fxml;
 	opens application to javafx.graphics, javafx.controls, javafx.fxml;
 }
